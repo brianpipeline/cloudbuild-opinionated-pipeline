f7f7345846e7d21ed53af72efd96828466cdf520
Brian Kurilko <briankurilko@gmail.com>
2024-03-29 16:03:18 -0400

    Trying a copilot fix, idk

--- pipelines/test-pipeline/tests/submissions.sh
+++ pipelines/test-pipeline/tests/submissions.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-gcloud builds submit . --config test-pipeline.yaml --substitutions "_SUB_VALUE=howdy,_SHORT_BUILD_ID="buildid"" --region=us-central1
+gcloud builds submit . --config test-pipeline.yaml --substitutions "_SUB_VALUE=howdy,_SHORT_BUILD_ID=\"buildid\"" --region=us-central1
