cbb613125455bcd9dabcd788c76a7812d1072dff
Brian Kurilko <briankurilko@gmail.com>
2024-03-29 15:45:34 -0400

    Fixed broken tests

--- pipelines/anothertest-pipeline/tests/submissions.sh
+++ pipelines/anothertest-pipeline/tests/submissions.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-gcloud builds submit . --config anotherTest.yaml --substitutions _SUB_VALUE=wassup --region=us-central1
+gcloud builds submit . --config anotherTest.yaml --substitutions "_SUB_VALUE=wassup,_SHORT_BUILD_ID="build id"" --region=us-central1
